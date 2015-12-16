d8d2d74790109b92c471327e2b12852f6d5a58f3
siad007 <siad.ardroumli@gmail.com>
2015-12-16 23:02:29 +0100

    Added bucket

--- src/Cache/Storage/Adapter/CouchbaseResourceManager.php
+++ src/Cache/Storage/Adapter/CouchbaseResourceManager.php
@@ -300,10 +300,6 @@ class CouchbaseResourceManager
 
     public function getBucket($id)
     {
-        if (!$this->hasResource($id)) {
-            return $this->getResource($id);
-        }
-
         $resource = &$this->resources[$id];
         return $resource['bucket'];
     }

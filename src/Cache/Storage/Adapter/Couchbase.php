58a59b68873fc14b0ba9433e1b0e1491747b63a0
siad007 <siad.ardroumli@gmail.com>
2015-12-19 17:31:45 +0100

    More work on cap

--- src/Cache/Storage/Adapter/Couchbase.php
+++ src/Cache/Storage/Adapter/Couchbase.php
@@ -254,7 +254,7 @@ class Couchbase extends AbstractAdapter implements FlushableInterface
                         'double'   => true,
                         'string'   => true,
                         'array'    => true,
-                        'object'   => 'array',
+                        'object'   => 'object',
                         'resource' => false,
                     ],
                     'supportedMetadata'  => [],
